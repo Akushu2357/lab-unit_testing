685f168e726c488b8262285a7fc46607c2b6310e
Akushu2357 <94792555+Akushu2357@users.noreply.github.com>
2024-11-19 03:10:00 +0000

    complete

--- calculator.py
+++ calculator.py
@@ -3,25 +3,45 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
+        if b < 0:
+            b = 0 - b
+            a = 0 - a
         result = 0
-        for i in range(b+1):
+        for i in range(b):
             result = self.add(result, a)
         return result
 
     def divide(self, a, b):
+        if (a < 0 and b >= 0) or \
+        (b < 0 and a >= 0):
+            step = -1
+            if b < 0:
+                b = 0 - b
+            else:
+                a = 0 - a
+        else:
+            step = 1
         result = 0
-        while a > b:
-            a = self.subtract(a, b)
-            result += 1
-        return result
+        if a >= b:
+            while a >= b:
+                a = self.subtract(a, b)
+                result += step
+            return result
+        else:
+            while a <= b:
+                a = self.subtract(a, b)
+                result += step
+            return result
     
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
-        return a
+        n = self.divide(a, b)
+        result = a - self.multiply(b, n)
+        if result < 0:
+            result = 0 - result
+        return result
 
 # Example usage:
 if __name__ == "__main__":
@@ -29,6 +49,6 @@ if __name__ == "__main__":
     print("This is a simple calculator class!")
     print("Example: addition: ", calc.add(1, 2))
     print("Example: subtraction: ", calc.subtract(4, 2))
-    print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
-    print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: multiplication: ", calc.multiply(2, -3))
+    print("Example: division: ", calc.divide(-10, -2))
+    print("Example: modulo: ", calc.modulo(-10, 3))
